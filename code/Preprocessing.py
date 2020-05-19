aa1ca006d1892066c8acba510701d0acddcec73b
Apurva <apurva.mulay4@gmail.com>
2020-05-18 22:32:35 -0400

    remove anonymous users

--- code/Preprocessing.py
+++ code/Preprocessing.py
@@ -5,11 +5,18 @@ df = pd.read_csv("../dataset/news-dataset_chicago_suntimes.csv", index_col=False
 remove_anonymous = ['Sun-Times Staff', 'Associated Press', 'Sun-Times Wire', 'Abigail Van Buren']
 
 
+def get_list_authors(authors):
+    # First 3 lines convert string to list. list_auth is the final list of authors
+    string_auth = authors[1:len(authors) - 1]
+    string_auth = string_auth.strip()
+    string_auth = string_auth.replace('\'', '')
+    list_auth = string_auth.split(",")
+    return list_auth
+
+
 def get_val(authors):
     if type(authors) == str:
-        string_auth = authors[1:len(authors) - 1]
-        string_auth = string_auth.replace('\'', '')
-        list_auth = string_auth.split(",")
+        list_auth = get_list_authors(authors)
         list_authors = []
 
         min_index = 100
@@ -28,8 +35,9 @@ def get_val(authors):
     return authors
 
 
-df['updated_authors'] = df.apply(lambda x: get_val(x['author']), axis=1)
+#df['updated_authors'] = df.apply(lambda x: get_val(x['author']), axis=1)
 df = df[~df.author.str.contains('Cst Editorial Board', na=False)]
+df = df[~df.updated_authors.str.contains('Letters To The Editor', na=False)]
 
 df.to_csv("../dataset/news-dataset_chicago_suntimes.csv", index=False)
 
@@ -46,7 +54,7 @@ def update_indexes(fileName):
 
 # Do not run this function
 def read_files_from_directory():
-    directory = os.path.join("/Users/apurvamulay/PycharmProjects/MIS-COV19/", "dataset/")
+    directory = os.path.join("../MIS-COV19/", "dataset/")
     print(directory)
     for root, dirs, files in os.walk(directory):
         for file in files:
@@ -59,8 +67,28 @@ def read_files_from_directory():
                 f.close()
 
 
-files = ['news-dataset_abc_news.csv', "news-dataset_business_insider.csv", "news-dataset_cbs_news.csv",
-         "news-dataset_chicago_suntimes.csv", "news-dataset_CNBC.csv", "news-dataset_fiveThirtyEight.csv",
-         "news-dataset_npr.csv", "news-dataset_pbs.csv"]
-for fileName in files:
-    update_indexes("../dataset/" + fileName)
+def update_index_for_all_files():
+    files = ['news-dataset_abc_news.csv', "news-dataset_business_insider.csv", "news-dataset_cbs_news.csv",
+             "news-dataset_chicago_suntimes.csv", "news-dataset_CNBC.csv", "news-dataset_fiveThirtyEight.csv",
+             "news-dataset_npr.csv", "news-dataset_pbs.csv"]
+    for fileName in files:
+        update_indexes("../dataset/" + fileName)
+
+
+def remove_values(authors, values):
+    if type(authors) == str:
+        list_authors = get_list_authors(authors)
+        list_authors = [x.strip() for x in list_authors]
+        res = [i.strip() for i in list_authors if i not in values]
+        return res
+    return authors
+
+
+def remove_specific_values(values):
+    df_csv = pd.read_csv("../dataset/news-dataset_chicago_suntimes.csv", index_col=False)
+    df_csv['updated_authors'] = df_csv.apply(lambda x: remove_values(x['updated_authors'], values), axis=1)
+    df_csv.to_csv("../dataset/news-dataset_chicago_suntimes.csv", index=False)
+    update_indexes("../dataset/news-dataset_chicago_suntimes.csv")
+
+
+remove_specific_values(['Usa Today', 'Better Government Association', 'Usa Today Network', 'Sun-Times Staff Report'])
