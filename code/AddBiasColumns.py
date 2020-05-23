7234ee94a1f7cc6cc4d77fb4ab892aceff4ccc3a
Apurva Mulay <apurva.mulay4@gmail.com>
2020-05-23 10:17:46 -0400

    Rename to AddBiasColumns.py

---
+++ code/AddBiasColumns.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import os
+import json
+
+columns = ['news_guard_score', 'mbfc_level', 'political_bias']
+CSV_FILE_DIR_HEAD = "../"
+
+REL_JSON_FILE = "reliable_web_statistics.json"
+UNREL_JSON_FILE = "unreliable_web_statistics.json"
+
+CSV_FILE_DIR_RELIABLE = CSV_FILE_DIR_HEAD + "dataset/reliable"
+CSV_FILE_DIR_UNRELIABLE = CSV_FILE_DIR_HEAD + "dataset/unreliable"
+
+
+def insert_statistics_columns(jsonfile, directory):
+    CSV_FILE_NAMES = os.listdir(directory)
+    with open(jsonfile) as json_file:
+        data = json.load(json_file)
+
+    for CSV_FILE_NAME in CSV_FILE_NAMES:
+        if CSV_FILE_NAME[:12] == "news-dataset":
+            fileName = directory + '/' + CSV_FILE_NAME
+            print(fileName)
+            df = pd.read_csv(fileName)
+            pub = data[df['publisher'][0]]
+            df['news_guard_score'] = pub['news_guard_score']
+            df['mbfc_level'] = pub['mbfc_level']
+            df['political_bias'] = pub['political_bias']
+            df.to_csv(fileName, index=False)
+
+
+# Reliable websites
+#insert_statistics_columns(REL_JSON_FILE, CSV_FILE_DIR_RELIABLE)
+
+# Unreliable websites
+insert_statistics_columns(UNREL_JSON_FILE, CSV_FILE_DIR_UNRELIABLE)
